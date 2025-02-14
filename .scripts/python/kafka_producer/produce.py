7ec5da8c39b1b5b6b1eca0668cc167b4df29e011
Song Li <da_tou_li@yahoo.com>
2025-02-14 16:40:56 -0500

    Updated the kafka-connector-example

--- .scripts/python/kafka_producer/produce.py
+++ .scripts/python/kafka_producer/produce.py
@@ -1,19 +1,37 @@
-def publish_to_kafka(responses):
-    topic = args['kafka_topic_name']
+import logging
+from datetime import datetime, timezone
+from producer import KafkaProducer
+from confluent_kafka.schema_registry.avro import AvroSerializer
+
+logging.basicConfig(level=logging.INFO)
+
+
+def get_kafka_configs():
+    # key_schema = load_schema(args['kafka_key_schema_file'])
+    # value_schema = load_schema(args['kafka_value_schema_file'])
+
+    # cert_file = args['cert_file']
+    # kafka_secret_name = args['kafka_configs_secret_name']
+
+    return {}, 'Topic_name', 'key_schema', 'value_schema'
+
+
+def publish_to_kafka(response):
+    kafka_configs, topic, key_schema, value_schema = get_kafka_configs()
+
     print(f'Starting to publish to kafka topic {topic}')
 
-    key_schema = load_schema(args['kafka_key_schema_file'])
-    value_schema = load_schema(args['kafka_value_schema_file'])
-
-    cert_file = args['cert_file']
-    kafka_secret_name = args['kafka_configs_secret_name']
-    schema_registry_client = KafkaProducer.initialize_schema_registry_client(kafka_secret_name, cert_file)
+    schema_registry_client = KafkaProducer.initialize_schema_registry_client(kafka_configs)
     key_serializer = AvroSerializer(schema_registry_client, key_schema)
     value_serializer = AvroSerializer(schema_registry_client, value_schema)
-    producer = KafkaProducer(kafka_secret_name, cert_file)
+
+    producer = KafkaProducer(kafka_configs)
 
     key = {'id': int(datetime.now(timezone.utc).timestamp())}
-    for response in responses:
-        producer.produce_message(topic, key, response, key_serializer, value_serializer)
+    producer.produce_message(topic, key, response, key_serializer, value_serializer)
 
     producer.flush_messages()
+
+
+if __name__ == '__main__':
+    print('OK')
--- .scripts/python/kafka_producer/producer.py
+++ .scripts/python/kafka_producer/producer.py
@@ -1,7 +1,4 @@
-import json
 import logging
-import boto3
-from botocore.exceptions import ClientError
 from confluent_kafka import Producer
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.serialization import SerializationContext, MessageField
@@ -10,12 +7,10 @@ logger = logging.getLogger()
 
 
 class KafkaProducer:
-    def __init__(self, secret_name, certificate_path):
+    def __init__(self, kafka_configs):
         logger.info('Inside init')
 
-        kafka_configs = get_secret(secret_name)
-
-        self.certificate_path = certificate_path
+        self.certificate_path = kafka_configs['certificate_path']
         self.kafka_password = kafka_configs['password']
         self.kafka_username = kafka_configs['username']
         self.kafka_bootstrap = kafka_configs['bootstrap_servers']
@@ -50,18 +45,6 @@ class KafkaProducer:
                 f"Message delivered to Topic - {msg.topic()} Partition - [{msg.partition()}] Offset - {msg.offset()}")
 
     def produce_message(self, topic, key, value, key_serializer, value_serializer, flush_messages=True):
-        """
-            Produces a message to the specified Kafka topic.
-            Args:
-                topic (str): The Kafka topic to which the message will be produced.
-                key (Any): The key of the message.
-                value (Any): The value of the message.
-                key_serializer (Callable): A function to serialize the key.
-                value_serializer (Callable): A function to serialize the value.
-                flush_messages (bool, optional): Whether to flush messages immediately after producing. Defaults to True.
-            Raises:
-                Exception: If there is an error producing the message to the topic.
-        """
         try:
 
             self.producer.produce(topic=topic,
@@ -86,41 +69,14 @@ class KafkaProducer:
             raise e
 
     @staticmethod
-    def initialize_schema_registry_client(secret_name, certificate_path):
-        """
-            Initialize the Schema Registry Client using the provided secret name and certificate path.
-            This function retrieves Kafka configurations from a secret store and uses them to configure
-            the Schema Registry Client. It handles the necessary SSL and authentication settings.
-            Args:
-                secret_name (str): The name of the secret containing Kafka configurations.
-                certificate_path (str): The file path to the SSL certificate.
-            Returns:
-                SchemaRegistryClient: An instance of the SchemaRegistryClient configured with the provided settings.
-            Raises:
-                Exception: If there is an error during the initialization of the Schema Registry Client.
-        """
-        kafka_configs = get_secret(secret_name)
-
+    def initialize_schema_registry_client(kafka_configs):
         try:
             schema_registry_conf = {
                 'url': kafka_configs['schema_registry_url'],
-                'ssl.ca.location': certificate_path,
+                'ssl.ca.location': kafka_configs['certificate_path'],
                 'basic.auth.user.info': kafka_configs['username'] + ":" + kafka_configs['password']
             }
             return SchemaRegistryClient(schema_registry_conf)
         except Exception as err:
             logger.error('Exception while initializing Schema Registry Client. Error is : {}'.format(err))
             raise err
-
-
-secret_manager_client = boto3.client('secretsmanager', region_name='us-east-1')
-
-
-def get_secret(secret_name):
-    try:
-        response = secret_manager_client.get_secret_value(SecretId=secret_name)
-        secret = json.loads(response['SecretString'])
-        return secret
-    except ClientError as e:
-        logger.error(f'Error retrieving secret {secret_name}: {e}')
-        raise e
